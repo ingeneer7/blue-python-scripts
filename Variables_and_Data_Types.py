c554e3c7d44586a4824f76c2b3e3d9d8c700d69a
ingeneer7 <52728909+ingeneer7@users.noreply.github.com>
2022-12-13 07:58:31 -0500

    Update Variables_and_Data_Types.py

--- Variables_and_Data_Types.py
+++ Variables_and_Data_Types.py
@@ -35,3 +35,74 @@ del user["family_name"]
 print(user)
 
 #Creating, Reading, Updating and Deleting elements in a list
+
+#Create Lists
+fruit = ["strawberries","grapes","watermelon"]
+
+#Create empty list by assigning [] to a variable
+fruit = []
+
+#Create empty list by using the list() constructor
+fruit = list()
+
+#Read Lists
+fruit = ["strawberries","grapes","watermelon"]
+print(fruit[1])
+
+#Find the length of a list
+len(fruit)
+
+#Return the last value in a list using a negative index value
+print(fruit[-1])
+
+print(fruit[-2])
+
+#Update Lists
+fruit.append("apples")
+print(fruit)
+
+#Add an element at a specific point in the list using the index value with the insert() method
+fruit.insert(2, "mangos")
+print(fruit)
+
+#Organizing a List
+
+#Return information while retaining the original order of the list with the sorted() function
+print(sorted(fruit))
+
+print(fruit)
+
+#Permanently sort the list using the sort() method
+fruit.sort()
+print(fruit)
+
+#Reverse the order of a list with the reverse() method
+fruit.reverse()
+print(fruit)
+
+#Delete Lists
+del fruit[1]
+print(fruit)
+
+#Using the value after removing it from a list with the pop() method
+favorite_fruit = fruit.pop()
+print(favorite_fruit)
+
+#Return any element with the pop() method by using the index value
+fresh_fruit = fruit.pop(1)
+print(fresh_fruit)
+
+#Use the remove() method to specify the value of the element you want to remove
+fruit.remove('mangos')
+print(fruit)
+
+#Determining Type
+
+#Find out what type of data python has stored in a variable using the type() method
+sample_variable = "My string"
+print(type(sample_variable))
+
+#Fix the TypeError by telling python to convert my_number to a string
+my_number = 11
+sample_string = "The number is "
+print(sample_string + str(my_number))
