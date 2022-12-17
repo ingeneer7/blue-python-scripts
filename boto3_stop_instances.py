717a644dc9cf5f3035a33aa2947ae0c42bc19fe7
“India <india.rogers89@gmail.com>
2022-12-17 18:19:06 +0000

    Added additional code to boto3 stop instance script

--- boto3_stop_instances.py
+++ boto3_stop_instances.py
@@ -9,7 +9,22 @@ for reservation in response['Reservations']:
     for instance in reservation['Instances']:
         print("Running Instance Image ID: {} Running instance Instance Type: {} Public IP Address: {} Private IP Address: {}"
               .format(instance['InstanceId'],instance['InstanceType'],instance['PublicIpAddress'],instance['PrivateIpAddress']))
+
     
-#Script that stops running EC2 instance
-ec2_list=['i-044f26b9df0f5dbaf', 'i-0c5924fd778cb2fa1']
-ec2.instances.filter(Filters=ec2_list).stop()
+#Script that stops all running EC2 instances
+ec2 = boto3.resource("ec2")
+
+for instance in ec2.instances.all():
+    instance.stop()
+
+#Script that stops running EC2 instances witht the dev tag
+ec2 = boto3.resource("ec2")
+
+ec2_running = {'Name': 'instance-state-name', 'Values': ['running']}
+dev_tag = {'Name': 'tag:environment', 'Values': ['dev']}
+
+for instance in ec2.instances.filter(Filters=[dev_tag]):
+    instance.stop()
+    print("The following EC2 instances are now stopping:", instance.id)
+    
+    
