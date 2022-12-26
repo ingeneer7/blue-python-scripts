33d40c2dee6f69a645b47a112b2a27677cda9ade
ingeneer7 <52728909+ingeneer7@users.noreply.github.com>
2022-12-26 16:55:02 -0500

    Update sqs_lamba_api.py

--- sqs_lamba_api.py
+++ sqs_lamba_api.py
@@ -9,3 +9,28 @@ queue = sqs.create_queue(QueueName='boto3-lambda-queue')
 
 # You can now access identifiers
 print(queue.url)
+
+###########################################################
+
+#Modifies the Lambda Function to include date and time in json output
+
+import json
+import boto3
+
+from datetime import datetime
+
+
+def lambda_handler(event, context):
+ 
+    date_time = datetime.now()
+    current_time = date_time.strftime("%H:%M:%S %p")
+
+    sqs = boto3.client('sqs')
+    sqs.send_message(
+        QueueUrl="https://sqs.us-east-1.amazonaws.com/256362179396/boto3-lambda-queue",
+        MessageBody=("This message was delivered on: " + str(date_time.strftime('%Y-%m-%d %H:%M:%S')))
+    )
+    return {
+        'statusCode': 200,
+        'body': json.dumps(current_time)
+    }
